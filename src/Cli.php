5b5d0520215bfba4f69d5d491164ac6f47cf20f4
Ksenia Krokhaleva <karibzhanova55@yandex.ru>
2023-10-26 13:17:14 +0500

    add user greeting function

--- src/Cli.php
+++ src/Cli.php
@@ -1,3 +1,13 @@
 <?php
 
-print_r('Welcome to the Brain Games!');
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function greetUser()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+};
